491c96137d38602d98f4c10dd6a703383ec347db
Responsibleuk <james@we-are-responsible.com>
2023-08-22 15:39:05 +0000

    Add

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,12 @@
-  // Immediately show the preloader
-  document.addEventListener("DOMContentLoaded", function() {
-    document.body.classList.remove("loaded");
-  });
+window.addEventListener('load', function () {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('main');
 
-  // Hide the preloader after 1 second or when the site is fully loaded
-  window.addEventListener("load", function() {
-    setTimeout(function() {
-      document.body.classList.add("loaded");
-    }, 1000); // 1000 milliseconds (1 second) loading time
-  });
+    // Hide preloader and show main content after a delay
+    setTimeout(function () {
+        preloader.style.opacity = '0';
+        preloader.style.transform = 'translateY(-100%)';
+        mainContent.style.opacity = '1';
+        mainContent.style.transform = 'translateY(0)';
+    }, 1200); // Adjust the delay (in milliseconds) as needed
+});

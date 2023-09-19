369387ccc94344eb1f686adedfce94bc009130d3
Responsibleuk <james@we-are-responsible.com>
2023-09-19 16:03:50 +0000

    Add

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,23 @@
-// Function to open the modal
-function openModal() {
-    document.getElementById("modalOverlay").style.display = "flex";
-  }
-  
-  // Function to close the modal
-  function closeModal() {
-    document.getElementById("modalOverlay").style.display = "none";
-  }
-  
-  // Open the modal automatically after the modal content has loaded
-  window.addEventListener("load", openModal);
-  
-  // Close the modal when the close button is clicked
-  document.getElementById("closeModalButton").addEventListener("click", closeModal);
+// Get references to the modal and close button
+const modal = document.getElementById('myModal');
+const closeModalBtn = document.getElementById('closeModalBtn');
 
+// Function to close the modal
+closeModalBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Show the modal when the page loads
+window.onload = function() {
+    modal.style.display = 'block';
+}
+
+// Close the modal if the user clicks anywhere outside of it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+} 
 
 
 
